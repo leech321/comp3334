4cfd3a314c83f9913b111be1fe46ffb11deb79c4
leech321 <118114930+leech321@users.noreply.github.com>
2023-04-20 01:05:52 +0800

    Update joe123.php

--- comp3334/ClientPage/joe123.php
+++ comp3334/ClientPage/joe123.php
@@ -78,9 +78,6 @@
             <a class="nav-link active" href="../Home.php" style="color: white;">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./forum.php">Forum</a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link" href="./aboutus.php">About us</a>
         </li>
         </ul>     
